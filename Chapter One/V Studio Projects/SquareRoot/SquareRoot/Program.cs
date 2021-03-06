2f098b3b92e844a6e37098b8e103ab7bf8242aba
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-06 11:17:14 +0100

    Add files via upload

---
+++ Chapter One/V Studio Projects/SquareRoot/SquareRoot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 12345;
+            double value = Math.Sqrt(x);
+            Console.WriteLine(value);
+        }
+    }
+}
