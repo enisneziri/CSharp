c1e9de2b05efe1d907c1da1748828c8d6e5c75c9
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-06 11:19:20 +0100

    Add files via upload

---
+++ Chapter One/V Studio Projects/FirsLastName/FirsLastName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirsLastName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enis Neziri");
+        }
+    }
+}
