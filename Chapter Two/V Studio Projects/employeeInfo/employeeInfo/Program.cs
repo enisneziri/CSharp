38eb627a685771c6fe04b0eea87198f4d33070d2
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-07 02:13:31 +0100

    Add files via upload

---
+++ Chapter Two/V Studio Projects/employeeInfo/employeeInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace employeeInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Filan";
+            string lastName = "Fisteku";
+            sbyte age = 20;
+            char gender = 'm';
+            int id = 27560000;
+            Console.WriteLine(firstName + " " + lastName + " is " + age + " is a " + gender + " his id number is " + id);
+
+        }
+    }
+}
