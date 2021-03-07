136ea944548fc77a819abdddcbe962360370ae04
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-07 02:12:56 +0100

    Add files via upload

---
+++ Chapter Two/V Studio Projects/DeclearingVariables/DeclearingVariables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SeveralVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte Num1 = 97;
+            sbyte Num2 = -115;
+            ushort Num3 = 52130;
+            int Num4 = 4825932;
+            short Num5 = -10000;
+            ushort Num6 = 20000;
+            byte Num7 = 224;
+            byte Num8 = 112;
+            sbyte Num9 = -44;
+            uint Num10 = 970700000;
+            int Num11 = -1000000;
+            ushort Num12 = 1990;
+            ulong Num13 = 123456789123456789;
+
+            Console.WriteLine("Byte:{0}, Sbyte:{1}, Ushort:{2}, Int:{3}, Short:{4}, ushort:{5}, byte:{6}, Byte:{7}," +
+                " sByte:{8}, Uint:{9}, Int:{10}, ushort:{11}, ulong:{12}",
+                Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9, Num10, Num11, Num12, Num13);
+
+
+        }
+    }
+}
