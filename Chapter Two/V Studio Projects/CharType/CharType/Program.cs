4f7cff826013c55e8bf2c247dd65d33cc63f242f
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-07 02:06:08 +0100

    Add files via upload

---
+++ Chapter Two/V Studio Projects/CharType/CharType/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CharType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            char CodeLetter = '\u0072';
+            Console.WriteLine(CodeLetter);
+        }
+    }
+}
