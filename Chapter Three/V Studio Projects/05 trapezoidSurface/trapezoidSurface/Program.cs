b4ac710d4510b39390a1ccf2c695f840a5494bd7
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-26 13:06:41 +0100

    Add files via upload

---
+++ Chapter Three/V Studio Projects/05 trapezoidSurface/trapezoidSurface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace trapezoidSurface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shkruaj informatat e Trapezit tuaj:");
+            Console.Write("Shkruaj Brinjen a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Shkruaj Brinjen b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Shkruaj Brinjen h: ");
+            int h = Convert.ToInt32(Console.ReadLine());
+            int formula = (a + b) * h / 2;
+            Console.WriteLine("Syprina e Trapezit eshte {0}",formula);
+        }
+    }
+}
---
+++ Chapter Three/V Studio Projects/06 perimeterRactangle/racktangelPandS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace racktangelPandS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            Console.Write("Shkruaj Brinjen b; ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int perimetri = (2 * a) + (2 * b);
+            int syprina = a * b;
+            Console.WriteLine("Perimetri i drejtkendeshit eshte {0}, kurse syprina eshte {1}", perimetri,syprina);
+        }
+    }
+}
