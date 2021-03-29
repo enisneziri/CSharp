5b755670c82398287918012fd9b972c122b156e4
Enis <74664187+enisneziri@users.noreply.github.com>
2021-03-29 11:52:41 +0200

    Add files via upload

---
+++ Chapter Four/V Studio Projects/11 lengthIneger/11 lengthIneger/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _11_lengthIneger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for(int i = 1; i <= count; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
