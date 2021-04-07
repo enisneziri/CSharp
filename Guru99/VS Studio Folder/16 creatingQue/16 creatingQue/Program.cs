234332f320aba5c90946080f5b4f3e091d8257db
Enis <74664187+enisneziri@users.noreply.github.com>
2021-04-07 12:27:44 +0200

    Add files via upload

---
+++ Guru99/VS Studio Folder/16 creatingQue/16 creatingQue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace _16_creatingQue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue qt = new Queue();
+            qt.Enqueue(1);
+            qt.Enqueue(2);
+            qt.Enqueue(3);
+
+            foreach (Object obj in qt)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine(); Console.WriteLine();
+            Console.WriteLine("The number of elements in the Queue " + qt.Count);
+            Console.WriteLine("Does the Queue contain " + qt.Contains(3));
+        }
+    }
+}
---
+++ Guru99/VS Studio Folder/17 removingQue/17 removingQue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace _17_removingQue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue qt = new Queue();
+            qt.Enqueue(1);
+            qt.Enqueue(2);
+            qt.Enqueue(3);
+
+            qt.Dequeue();
+
+            foreach (Object obj in qt)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
