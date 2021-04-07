a0153ec1883e6f31fa0f1a59ddc779f33dfef6b9
Enis <74664187+enisneziri@users.noreply.github.com>
2021-04-07 12:21:41 +0200

    Add files via upload

---
+++ Guru99/VS Studio Folder/02 Integer/Integer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 200;//integer variable
+            Console.Write(num);
+        }
+    }
+}
---
+++ Guru99/VS Studio Folder/03 Double/Double/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = 50.55;//double variable
+            Console.WriteLine(num);
+        }
+    }
+}
