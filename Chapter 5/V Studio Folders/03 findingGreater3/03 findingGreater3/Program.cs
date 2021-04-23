902c29cf9c90c4ef890e39bae32dd650526aefa4
Enis <74664187+enisneziri@users.noreply.github.com>
2021-04-23 17:01:42 +0200

    Add files via upload

---
+++ Chapter 5/V Studio Folders/03 findingGreater3/03 findingGreater3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03_findingGreater3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write 3 numbers: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+                if (a > c) Console.WriteLine("A is the biggest");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A and C are the biggest");
+            else if (a < b)
+                if (b > c) Console.WriteLine("B is the biggest");
+                else if (b < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("B and C are the biggest");
+            else if (a == b)
+                if (a == c) Console.WriteLine("All are equal");
+                else if (a < c) Console.WriteLine("C is the biggest");
+                else Console.WriteLine("A and B are the biggest");
+            
+
+        }
+    }
+}
