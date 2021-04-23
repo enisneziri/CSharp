869efad267db1d1265b8d19179b66bed0c307d5e
Enis <74664187+enisneziri@users.noreply.github.com>
2021-04-23 17:02:43 +0200

    Add files via upload

---
+++ Chapter 5/V Studio Folders/01 greaterSwitchV/01 greaterSwitchV/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01_greaterSwitchV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Write the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                num2 = num1;
+                Console.WriteLine(num2);
+            }
+            else if(num2 > num1)
+            {
+                num1 = num2;
+                Console.WriteLine(num1);
+            }else if (num1 == num2)
+            {
+                Console.WriteLine("Numbers are equal");
+            }
+
+        }
+    }
+}
