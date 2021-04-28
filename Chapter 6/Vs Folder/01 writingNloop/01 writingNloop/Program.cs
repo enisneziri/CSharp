6645ca3c46760400e6c92066131fbd0abf0ac3df
Enis <74664187+enisneziri@users.noreply.github.com>
2021-04-28 18:14:51 +0200

    Add files via upload

---
+++ Chapter 6/Vs Folder/01 writingNloop/01 writingNloop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01_writingNloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shkruaj Nje numer ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 1; i < num; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Chapter 6/Vs Folder/02 devisebleBy7/02 devisebleBy7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02_devisebleBy7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shkruaj numrin ");
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                
+                if (i % (3 * 7) != 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
